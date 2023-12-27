79597d54a9cf05df58ffc7fbc6d441dcdf6eed24
eldm <eldm2350@protonmail.com>
2023-12-27 04:14:21 -0600

    Finished auth and most of styling

--- eldmnet/accounts/urls.py
+++ eldmnet/accounts/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 app_name = 'accounts'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='index'),
+    path('user_login/', views.user_login, name='user_login'),
+    path('register/', views.register, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', views.user_logout, name='logout'), 
 ]
--- eldmnet/accounts/views.py
+++ eldmnet/accounts/views.py
@@ -1,5 +1,53 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import UserForm
 
 # Create your views here.
-def index(request):
+def home(request):
     return render(request, 'accounts/base.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('accounts:index'))
+
+@login_required
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+def register(request):
+    registered = False
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            registered = True
+        else:
+            print(user_form.errors)
+    else:
+        user_form = UserForm()
+    return render(request, 'accounts/registration.html', {'user_form': user_form,
+                                                          'registered': registered})
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if(user):
+            if(user.is_active):
+                login(request, user)
+                return HttpResponseRedirect(reverse('accounts:dashboard'))
+            else:
+                return HttpResponse("account is not active.")
+        else:
+            print("Someone tried to login and failed.")
+            print("Username: {} and Password: {}".format(username, password))
+            return HttpResponse("invalid login details supplied!")
+    else:
+        return render(request, 'accounts/login.html', {})
